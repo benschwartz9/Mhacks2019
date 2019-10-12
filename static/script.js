a29eaf95e2de87b628af4ee4e3f96288f2b2e6e2
erichumphries <ehumphri@purdue.edu>
2019-10-12 18:30:02 -0400

    Website UI

--- static/script.js
+++ static/script.js
@@ -16,11 +16,28 @@
 // [START gae_python37_log]
 'use strict';
 
+document.getElementById("default").click();
+
 window.addEventListener('load', function () {
 
   console.log("Hello World!");
 
 });
+
+function showTabContent(evt, id) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+ 	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+ 	}
+  	tablinks = document.getElementsByClassName("tablinks");
+  	for (i = 0; i < tablinks.length; i++) {
+    	tablinks[i].className = tablinks[i].className.replace(" active", "");
+  	}
+
+  	document.getElementById(id).style.display = "block";
+  	evt.currentTarget.className += " active";
+}
 
  
 // [END gae_python37_log]
